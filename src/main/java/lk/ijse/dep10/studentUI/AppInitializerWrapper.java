4217b3b2a5d4dea038a82303f54c63d7f3b9e37a
Subhash-Chamara <udayanga.subash@gmail.com>
2023-03-23 18:37:31 +0530

    User Interface Implemented

---
+++ src/main/java/lk/ijse/dep10/studentUI/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep10.studentUI;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/studentUI/controll/StudentManageViewController.java
@@ -0,0 +1,53 @@
+package lk.ijse.dep10.studentUI.controll;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class StudentManageViewController {
+
+    @FXML
+    private Button brnSave;
+
+    @FXML
+    private Button btnAddNewStudent;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private TableView<?> tblStudents;
+
+    @FXML
+    private TextField txtAdddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void brnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddNewStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblStudentsOnKeyReleased(KeyEvent event) {
+
+    }
+
+}
