7b2f90c9a6941ce0587e1ee12ab8a5599f08ee39
danushka kasun <danushkakasun05@gmail.com>
2023-03-23 18:45:20 +0530

    finalized UI

---
+++ src/main/java/lk/ijse/dep10/Controller/EmployeeSceneController.java
@@ -0,0 +1,39 @@
+package lk.ijse.dep10.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EmployeeSceneController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblEmployee;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
