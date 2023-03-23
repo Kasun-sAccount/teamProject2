ed5b5946fecb73b9fa1253a4e46a8c216c33e84c
danushka kasun <danushkakasun05@gmail.com>
2023-03-24 00:35:09 +0530

    final submission close#5

--- src/main/java/lk/ijse/dep10/Controller/EmployeeSceneController.java
+++ src/main/java/lk/ijse/dep10/Controller/EmployeeSceneController.java
@@ -37,10 +37,10 @@ public class EmployeeSceneController {
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
         try {
-            Employee emp =tblEmployee.getSelectionModel().getSelectedItem();
-            Connection connection =DBConnector.getInstance().getConnection();
-            PreparedStatement preStm =connection.prepareStatement("delete from Employee where id =?");
-            preStm.setString(1,emp.getId());
+            Employee emp = tblEmployee.getSelectionModel().getSelectedItem();
+            Connection connection = DBConnector.getInstance().getConnection();
+            PreparedStatement preStm = connection.prepareStatement("delete from Employee where id =?");
+            preStm.setString(1, emp.getId());
             preStm.execute();
             tblEmployee.getItems().remove(emp);
 
@@ -48,7 +48,7 @@ public class EmployeeSceneController {
             btnNew.fire();
         } catch (SQLException e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR,"Fail to delete from database");
+            new Alert(Alert.AlertType.ERROR, "Fail to delete from database");
             throw new RuntimeException(e);
         }
 
@@ -57,14 +57,14 @@ public class EmployeeSceneController {
     @FXML
     void btnSaveOnAction(ActionEvent event) {
         try {
-            if(!validate()) return;
+            if (!validate()) return;
             String id = txtID.getText();
             String name = txtName.getText();
             String address = txtAddress.getText();
 
             Connection connection = DBConnector.getInstance().getConnection();
             connection.setAutoCommit(false);
-            if(tblEmployee.getSelectionModel().getSelectedItem()==null) {
+            if (tblEmployee.getSelectionModel().getSelectedItem() == null) {
                 PreparedStatement preStm = connection.prepareStatement("Insert into Employee value (?,?,?)");
 
                 preStm.setString(1, id);
@@ -75,19 +75,18 @@ public class EmployeeSceneController {
                 Employee employee = new Employee(id, name, address);
                 tblEmployee.getItems().add(employee);
 
-            }else if(tblEmployee.getSelectionModel().getSelectedItem()!=null) {
+            } else if (tblEmployee.getSelectionModel().getSelectedItem() != null) {
 
                 Employee emp = tblEmployee.getSelectionModel().getSelectedItem();
                 PreparedStatement preStm2 = connection.prepareStatement("update  Employee set name =?,address=? where id =?");
-                preStm2.setString(1,txtName.getText());
-                preStm2.setString(2,txtAddress.getText());
-                preStm2.setString(3,emp.getId());
+                preStm2.setString(1, txtName.getText());
+                preStm2.setString(2, txtAddress.getText());
+                preStm2.setString(3, emp.getId());
                 preStm2.execute();
 
-                Employee empNew = new Employee(txtID.getText(),txtName.getText(),txtAddress.getText());
-              tblEmployee.getItems().clear();
-              updateTable();
-
+                Employee empNew = new Employee(txtID.getText(), txtName.getText(), txtAddress.getText());
+                tblEmployee.getItems().clear();
+                updateTable();
 
 
             }
@@ -107,16 +106,17 @@ public class EmployeeSceneController {
 
 
     }
-    public void initialize(){
+
+    public void initialize() {
         btnDelete.setDisable(true);
         txtID.setDisable(true);
         updateTable();
         generateId();
 
 
-        tblEmployee.getSelectionModel().selectedItemProperty().addListener((value,previous,current)->{
-            btnDelete.setDisable(current==null);
-            if(current!=null){
+        tblEmployee.getSelectionModel().selectedItemProperty().addListener((value, previous, current) -> {
+            btnDelete.setDisable(current == null);
+            if (current != null) {
                 txtID.setText(current.getId());
                 txtName.setText(current.getName());
                 txtAddress.setText(current.getAddress());
@@ -127,36 +127,38 @@ public class EmployeeSceneController {
         tblEmployee.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tblEmployee.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
     }
-    public boolean validate(){
+
+    public boolean validate() {
         boolean validate = true;
-        if(!txtAddress.getText().matches(".{5,}")){
+        if (!txtAddress.getText().matches(".{5,}")) {
             txtAddress.requestFocus();
-            validate=false;
+            validate = false;
 
         }
-        if(!txtName.getText().strip().matches("[A-z ]{5,}")){
+        if (!txtName.getText().strip().matches("[A-z ]{5,}")) {
             txtName.requestFocus();
-            validate=false;
+            validate = false;
         }
         return validate;
     }
-    private void generateId(){
 
-        if(tblEmployee.getItems().isEmpty()){
+    private void generateId() {
+
+        if (tblEmployee.getItems().isEmpty()) {
             txtID.setText("E001");
-        }
-        else {
-            ObservableList<Employee >empList= tblEmployee.getItems();
-            txtID.setText(String.format("E%03d",Integer.parseInt(empList.get(empList.size()-1).getId().substring(1))+1));
+        } else {
+            ObservableList<Employee> empList = tblEmployee.getItems();
+            txtID.setText(String.format("E%03d", Integer.parseInt(empList.get(empList.size() - 1).getId().substring(1)) + 1));
         }
 
     }
-    private void updateTable(){
-        Connection connection =DBConnector.getInstance().getConnection();
+
+    private void updateTable() {
+        Connection connection = DBConnector.getInstance().getConnection();
         try {
             Statement statement = connection.createStatement();
-            ResultSet resultSet=statement.executeQuery("select * from Employee");
-            while (resultSet.next()){
+            ResultSet resultSet = statement.executeQuery("select * from Employee");
+            while (resultSet.next()) {
                 String id = resultSet.getString(1);
                 String name = resultSet.getString(2);
                 String address = resultSet.getString(3);
