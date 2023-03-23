50fbbd39e48626c800d5026a57979729dae5e423
danushka kasun <danushkakasun05@gmail.com>
2023-03-23 19:31:15 +0530

    connctionand databaseloading completed

--- src/main/java/lk/ijse/dep10/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/AppInitializer.java
@@ -1,7 +1,17 @@
 package lk.ijse.dep10;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import lk.ijse.dep10.db.DBConnector;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AppInitializer extends Application {
 
@@ -11,6 +21,40 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        checkTable();
+        try {
+            primaryStage.setScene(new Scene(new FXMLLoader(getClass().getResource("/View/EmployeeScene.fxml")).load()));
+            primaryStage.show();
+            primaryStage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Fail to load UI").showAndWait();
+            throw new RuntimeException(e);
+        }
+    }
+    public void checkTable(){
+        Connection connection = DBConnector.getInstance().getConnection();
+        try {
+            InputStream is = getClass().getResourceAsStream("/schema.sql");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line="";
+            StringBuilder sb = new StringBuilder();
+            while ((line= br.readLine())!=null){
+                sb.append(line).append("\n");
 
+            }
+            br.close();
+            System.out.println(sb.toString());
+            Statement statement =connection.createStatement();
+            statement.execute(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Fail to connect with schema file").showAndWait();
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Fail to connect with database server").showAndWait();
+            throw new RuntimeException(e);
+      }
     }
 }
--- src/main/java/lk/ijse/dep10/Controller/EmployeeSceneController.java
+++ src/main/java/lk/ijse/dep10/Controller/EmployeeSceneController.java
@@ -35,5 +35,6 @@ public class EmployeeSceneController {
     void btnSaveOnAction(ActionEvent event) {
 
     }
+    public void
 
 }
